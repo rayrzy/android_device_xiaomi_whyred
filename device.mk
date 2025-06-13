47e0204c800d69725351015900c0e03e556400da
rayrzy <vrayananda@yahoo.com>
2025-06-13 16:30:45 +0700

    whyred: drop overlay-pe
    Signed-off-by: rayrzy <vrayananda@yahoo.com>

--- device.mk
+++ device.mk
@@ -45,8 +45,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-pe
+    $(DEVICE_PATH)/overlay
 
 # Ramdisk
 PRODUCT_PACKAGES += \
